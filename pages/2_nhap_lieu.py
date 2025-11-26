ae10397710a03e3aaa86a9c402a6d43f7412d563
khoicpct-collab <khoicpct@gmail.com>
2025-11-26 11:19:01 +0700

    Create 2_nhap_lieu.py

---
+++ pages/2_nhap_lieu.py
@@ -0,0 +1,20 @@
+Page: Nhap lieu nguyen lieu
+
+1. Chon nguyen lieu (autocomplete, khong dau)
+2. Bang cac lo hien co:
+      Lo | So bao | So kg | Avg | Ngay nhap | Tuoi | Ton cuoi
+3. Form Nhap moi:
+      - Chon lo
+      - So bao nhap
+      - So kg nhap
+      - Ngay nhap
+      - Ngay SX
+      - NCC
+      - Code
+      - Ghi chu
+      - NUT [LUU]
+4. Form Xuat:
+      - Chon lo
+      - So bao xuat
+      - So kg xuat
+      - NUT [LUU]
